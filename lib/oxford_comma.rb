add928590089b4f621ce10fa9922a8fd6bb13814
kcluniesa <kcluniesa@yahoo.com>
2018-10-31 00:39:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2 
-    array.join(" and ")
-    x = "and" + array[array.length-1]
+    array.join
+    array[array.length-1] = "and" + array[array.length-1]
   else array.join(", ") << x
   end
 end
