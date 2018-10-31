744022793373ad37d1905e6470710cf128aa8d84
kcluniesa <kcluniesa@yahoo.com>
2018-10-31 00:04:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ if array.length == 1
   array.join
 elsif array.length == 2 
 array.join(" and ")
-else array-1.join(", ")
+else array...join(", ")
 #else array.join(", ") << array.length-1["and"]
 end
 end
